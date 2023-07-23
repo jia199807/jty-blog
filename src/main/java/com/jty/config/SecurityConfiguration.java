474805a7da0c16afd4ab91b0acf28ee41a628a6f
jty <642096532@qq.com>
2023-07-23 15:30:39 +0800

    改变了jwt生成方式 完成了AuthenticationSuccessHandler配置

--- src/main/java/com/jty/config/SecurityConfiguration.java
+++ src/main/java/com/jty/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.jty.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jty.domain.entity.LoginUser;
 import com.jty.domain.entity.User;
 import com.jty.domain.entity.vo.BlogUserLoginVo;
 import com.jty.domain.entity.vo.UserInfoVo;
@@ -14,12 +15,12 @@ import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,6 +28,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 @Configuration
 @EnableWebSecurity// 开启网络安全注解
@@ -92,11 +94,15 @@ public class SecurityConfiguration {
             PrintWriter out = resp.getWriter();
 
 
-            User user = (User) auth.getPrincipal();
-            // 重新封装auth中的User
-            String jwtToken = JwtUtil.generateToken((UserDetails) user);
+            // 获取userid 生成token
+            LoginUser loginUser = (LoginUser) auth.getPrincipal();
+            User user = loginUser.getUser();
+            String userId = loginUser.getUser().getId().toString();
+
+            String jwtToken = JwtUtil.buildToken(new HashMap<>(), userId);
+
             // 把用户信息存入redis
-            redisCache.setCacheObject("blog:" + user.getId(), user);
+            redisCache.setCacheObject("blog:" + userId, loginUser);
 
             // 把User转换成UserInfoVo
             UserInfoVo userInfoVo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
@@ -116,18 +122,20 @@ public class SecurityConfiguration {
         filter.setAuthenticationFailureHandler((req, resp, e) -> {
             resp.setContentType("application/json;charset=utf-8");
             PrintWriter out = resp.getWriter();
-            ResponseResult result = ResponseResult.errorResult("登录失败");
-            if (e instanceof BadCredentialsException) {
-                result.setMsg("用户名或者密码输入错误，登录失败");
-            } else if (e instanceof DisabledException) {
-                result.setMsg("账户被禁用，登录失败");
-            } else if (e instanceof CredentialsExpiredException) {
-                result.setMsg("密码过期，登录失败");
-            } else if (e instanceof AccountExpiredException) {
-                result.setMsg("账户过期，登录失败");
-            } else if (e instanceof LockedException) {
-                result.setMsg("账户被锁定，登录失败");
-            }
+            e.printStackTrace();
+
+            ResponseResult result = ResponseResult.errorResult("用户名或者密码输入错误，登录失败");
+            // if (e instanceof BadCredentialsException) {
+            //     result.setMsg("用户名或者密码输入错误，登录失败");
+            // } else if (e instanceof DisabledException) {
+            //     result.setMsg("账户被禁用，登录失败");
+            // } else if (e instanceof CredentialsExpiredException) {
+            //     result.setMsg("密码过期，登录失败");
+            // } else if (e instanceof AccountExpiredException) {
+            //     result.setMsg("账户过期，登录失败");
+            // } else if (e instanceof LockedException) {
+            //     result.setMsg("账户被锁定，登录失败");
+            // }
             out.write(new ObjectMapper().writeValueAsString(result));
         });
         filter.setAuthenticationManager(authenticationManager());
